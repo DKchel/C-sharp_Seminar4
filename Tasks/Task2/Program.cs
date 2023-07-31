0bd2cf6dc3ae98a691b0a0928029f39eca76b45f
KDA <kuda74ru@gmail.ru>
2023-07-31 10:57:45 +0500

    add task2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int GetSumNum()
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+
+    return count;
+}
+
+int result = GetSumNum();
+Console.WriteLine(result);
