12deed7e2076ed3a750a22a28eeb5e36fecfa3cd
KDA <kuda74ru@gmail.ru>
2023-08-02 21:43:37 +0500

    add hometask2

---
+++ Homework/HomeTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int GetSumDigit()
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+Console.WriteLine(GetSumDigit());
