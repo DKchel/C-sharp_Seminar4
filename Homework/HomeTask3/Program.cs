bdfb8201f357f9dc37a16cc9367bff476d9d6658
KDA <kuda74ru@gmail.ru>
2023-08-04 09:14:15 +0500

    add min/max to hometask3

--- Homework/HomeTask3/Program.cs
+++ Homework/HomeTask3/Program.cs
@@ -2,13 +2,19 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+Console.WriteLine("Введите минимум случайных числел массива:");
+int arrayMin = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимум случайных числел массива:");
+int arrayMax = Convert.ToInt32(Console.ReadLine());
+
 int[] array = new int[8];
 
 void GetArray()
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0, 100);  // тут не понял ДЗ, поставил рандом до 99, можно также array[i] = new Random().Next(); для рандома без ограничений
+        array[i] = new Random().Next(arrayMin, arrayMax);
         Console.Write(array[i] + " ");
     }
 }
